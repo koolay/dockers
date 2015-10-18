a8444c7680877d0d76916eb2b450001cd1b8da6c
hwl <koolay@163.com>
2015-10-18 20:12:57 +0800

    add code

---
+++ alpine-php/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/ash
+
+set -eu
+
+DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
+echo "$DOCKER_HOST_IP localhost" >> /etc/hosts
+
+"$@"
---
+++ alpine-postgres/README.md
@@ -0,0 +1,1 @@
+This repo builds a docker image that accepts the same env vars as the official postgres build but with a much smaller footprint
