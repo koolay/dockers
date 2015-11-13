fa101bd821074ca05ab4abcc3ff22d51e54c816e
huwl <huwl@mysoft.com.cn>
2015-11-13 14:08:40 +0800

    add hosts

--- alpine-php/entrypoint.sh
+++ alpine-php/entrypoint.sh
@@ -3,6 +3,6 @@
 set -eu
 
 DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
-echo "$DOCKER_HOST_IP localhost" >> /etc/hosts
+echo "$DOCKER_HOST_IP dev.myapp.com" >> /etc/hosts
 
 "$@"
