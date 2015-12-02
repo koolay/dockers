5b19953991457761009a2a4c1897d6a93915afdf
koolay <koolay@163.com>
2015-12-02 23:13:32 +0800

    fpm docker

--- php-fpm/init.sh
+++ php-fpm/init.sh
@@ -6,6 +6,5 @@ DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
 echo "$DOCKER_HOST_IP dev.myapp.com" >> /etc/hosts
 
 sed 's/;daemonize = yes/daemonize = no/' -i /etc/php5/fpm/php-fpm.conf
-sed 's/;session.save_path = "/var/lib/php5"/;session.save_path=$DOCKER_HOST_IP:$MEMCACHED_PORT/' -i /etc/php5/fpm/php.ini
 
 "$@"
