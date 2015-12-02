9c46cb88378d0878439026842df70afc06bec584
koolay <koolay@163.com>
2015-12-02 22:41:43 +0800

    memcached host

--- php-fpm/init.sh
+++ php-fpm/init.sh
@@ -5,7 +5,7 @@ set -eu
 DOCKER_HOST_IP=$(netstat -nr | grep '^0\.0\.0\.0' | awk '{print $2}')
 echo "$DOCKER_HOST_IP dev.myapp.com" >> /etc/hosts
 
-RUN sed 's/;daemonize = yes/daemonize = no/' -i /etc/php5/fpm/php-fpm.conf
-RUN sed 's/;session.save_path = "/var/lib/php5"/;session.save_path=$DOCKER_HOST_IP:$MEMCACHED_PORT/' -i /etc/php5/fpm/php.ini
+sed 's/;daemonize = yes/daemonize = no/' -i /etc/php5/fpm/php-fpm.conf
+sed 's/;session.save_path = "/var/lib/php5"/;session.save_path=$DOCKER_HOST_IP:$MEMCACHED_PORT/' -i /etc/php5/fpm/php.ini
 
 "$@"
